e810b1433d727c9fb39e208d5035ca01a19e2dfd
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-06 04:27:30 -0700

    Simple solution with if conditions.

---
+++ 13.RomanToInteger/Program.cs
@@ -0,0 +1,11 @@
+﻿using _13.RomanToInteger;
+
+var solution = new Solution();
+var result = solution.RomanToInt("III");
+Console.WriteLine($"Expected 3. Got: {result}");
+
+result = solution.RomanToInt("LVIII");
+Console.WriteLine($"Expected 58. Got: {result}");
+
+result = solution.RomanToInt("MCMXCIV");
+Console.WriteLine($"Expected 1994. Got: {result}");
---
+++ 13.RomanToInteger/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace _13.RomanToInteger;
+
+public class Solution
+{
+    Dictionary<char, int> map = new Dictionary<char, int>()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+
+    public int RomanToInt(string s)
+    {
+        int result = 0;
+        char currentSymbol, prevSymbol;
+
+        for(int i = s.Length - 1; i >= 0; --i)
+        {
+            currentSymbol = s[i];
+            result += map[currentSymbol];
+            if(currentSymbol == 'I' && i != s.Length - 1)
+            {
+                prevSymbol = s[i + 1];
+                if(prevSymbol == 'V' || prevSymbol == 'X')
+                {
+                    result -= 2;
+                    continue;
+                }
+            }
+            else if(currentSymbol == 'X' && i != s.Length - 1)
+            {
+                prevSymbol = s[i + 1];
+                if(prevSymbol == 'L' || prevSymbol == 'C')
+                {
+                    result -= 20;
+                    continue;
+                }
+            }
+            else if (currentSymbol == 'C' && i != s.Length - 1)
+            {
+                prevSymbol = s[i + 1];
+                if (prevSymbol == 'D' || prevSymbol == 'M')
+                {
+                    result -= 200;
+                    continue;
+                }
+            }
+        }
+
+        return result;
+    }
+}
