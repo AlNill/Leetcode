e8e64797b7fd38310be334403479c119c4e8cd22
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-08 12:47:49 -0700

    Good solution

---
+++ 27.RemoveElement/Program.cs
@@ -0,0 +1,14 @@
+﻿using _27.RemoveElement;
+
+var s = new Solution();
+int[] test = new int[] { 3, 2, 2, 3 };
+int k = s.RemoveElement(test, 3);
+Console.WriteLine($"Expected: 2. Out: {k}");
+
+test = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+k = s.RemoveElement(test, 2);
+Console.WriteLine($"Expected: 5. Out: {k}");
+
+test = new int[] { 2 };
+k = s.RemoveElement(test, 3);
+Console.WriteLine($"Expected: 1. Out: {k}");
