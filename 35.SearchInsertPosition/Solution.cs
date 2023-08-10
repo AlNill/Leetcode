a8aa01a14f7fa963ed3807c6d08213bd4464a994
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-10 09:02:27 -0700

    Good solution throut Binary Search with complex O(logn)

--- 35.SearchInsertPosition/Solution.cs
+++ 35.SearchInsertPosition/Solution.cs
@@ -8,15 +8,13 @@ public class Solution
         while(l <= r)
         {
             m = (l + r) / 2;
-            if (l == r)
-                return m + 1;
-            else if (nums[m] < target)
+            if (nums[m] < target)
                 l = m + 1;
             else if (nums[m] > target)
                 r = m - 1;
             else
                 return m;
         }
-        return nums.Length;
+        return l;
     }
 }
