d6d3d0b9790d4928f79f87dea14909a49d3e3e05
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-14 11:00:21 -0700

    Good solution

---
+++ 58.LengthOfLastWord/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace _58.LengthOfLastWord;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        int i = s.Length - 1, len = 0;
+        while (i >= 0 && s[i] == ' ')
+        {
+            --i;
+        }
+
+        while(i >= 0 && s[i] != ' ')
+        {
+            ++len;
+            --i;
+        }
+        return len;
+    }
+}
