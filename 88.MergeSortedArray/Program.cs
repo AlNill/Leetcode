b52bea9d1a8ccc1bdd7f619a4046468f03d42d8f
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-08-23 12:51:53 -0700

    O(n+m)

---
+++ 88.MergeSortedArray/Program.cs
@@ -0,0 +1,7 @@
+﻿using _88.MergeSortedArray;
+
+var s = new Solution();
+int[] nums1 = new int[6] { 1, 2, 3, 0, 0, 0 }; 
+int[] nums2 = new int[3] { 2, 5, 6 };
+s.Merge(nums1, 3, nums2, 3);
+Console.WriteLine(nums1);
