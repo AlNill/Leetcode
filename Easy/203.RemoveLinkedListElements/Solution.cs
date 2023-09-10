5e36d7253dedf2d69daf9db464cfc0ef38a2bdba
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-10 07:45:34 -0700

    Good

---
+++ Easy/203.RemoveLinkedListElements/Solution.cs
@@ -0,0 +1,34 @@
+﻿using Easy.Common;
+
+namespace Easy._203.RemoveLinkedListElements;
+
+/*
+ * Beats 85.76% by speed and Beats 47.22% by memory
+ */
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        while (head != null && head.val == val)
+        {
+            head = head.next;
+        }
+
+        var tmp = head;
+        while (tmp != null && tmp.next != null)
+        {
+            if (tmp.next.val == val)
+            {
+                var tmp2 = tmp.next;
+                while (tmp2 != null && tmp2.val == val)
+                {
+                    tmp2 = tmp2.next;
+                }
+
+                tmp.next = tmp2;
+            }
+            tmp = tmp.next;
+        }
+        return head;
+    }
+}
