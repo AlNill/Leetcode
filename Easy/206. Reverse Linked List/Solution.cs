a8d2c37e1828f8784b3af6ce999017485f4969ae
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-11 11:36:51 -0700

    Good

---
+++ Easy/206. Reverse Linked List/Solution.cs
@@ -0,0 +1,24 @@
+﻿using Easy.Common;
+
+namespace Easy._206._Reverse_Linked_List;
+
+/*
+ * Beats 97.30% by speed and Beats 84.99% by memory
+ */
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+        ListNode prev = null;
+        while (head != null)
+        {
+            var current = head;
+            head = head.next;
+            current.next = prev;
+            prev = current;
+        }
+        return prev;
+    }
+}
