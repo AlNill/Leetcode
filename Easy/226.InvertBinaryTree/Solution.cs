3b1eb9ce6467d5f3246e8d581ac86c41b7514562
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-13 11:27:47 -0700

    Good

---
+++ Easy/226.InvertBinaryTree/Solution.cs
@@ -0,0 +1,22 @@
+﻿using Easy.Common;
+
+namespace Easy._226.InvertBinaryTree;
+
+/*
+ * Beats 97.96 by speed and 57.23% by memory;
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+            return null;
+
+        var tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
