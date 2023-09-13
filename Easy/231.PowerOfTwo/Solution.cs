a6ef8310b218f09040c9add9e58092549d473769
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-13 11:25:54 -0700

    Good

---
+++ Easy/231.PowerOfTwo/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Easy._231.PowerOfTwo;
+
+/*
+ * Beats 79.67% by speed and 47.74% by memory
+ */
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n < 0)
+            return false;
+        bool isExistOne = false;
+        for (int i = 0; i < 31; ++i)
+        {
+            int lastBit = n & 1;
+            if (lastBit == 1)
+            {
+                if (isExistOne)
+                    return false;
+                isExistOne = true;
+            }
+            n = n >> 1;
+        }
+        return isExistOne == true ? true : false;
+    }
+}
