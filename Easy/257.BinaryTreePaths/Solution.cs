b999808564b785bc7adbbbd1f4868addc6a9d675
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-17 11:17:03 -0700

    Good by speed

---
+++ Easy/257.BinaryTreePaths/Solution.cs
@@ -0,0 +1,45 @@
+﻿using Easy.Common;
+
+namespace Easy._257.BinaryTreePaths;
+
+/*
+ * Beats 84.68% by speed and 40.54% by memeory
+ */
+public class Solution
+{
+    private bool IsLeaf(TreeNode root)
+    {
+        return root.left == null && root.right == null;
+    }
+
+    private void PathToLeaf(TreeNode root, string path, IList<string> result)
+    {
+        if (IsLeaf(root))
+        {
+            result.Add(path += $"->{root.val}");
+            return;
+        }
+
+        if (root.left != null)
+            PathToLeaf(root.left, path + $"->{root.val}", result);
+        if (root.right != null)
+            PathToLeaf(root.right, path + $"->{root.val}", result);
+    }
+
+    public IList<string> BinaryTreePaths(TreeNode root)
+    {
+        IList<string> result = new List<string>();
+        if (root.left == null && root.right == null)
+        {
+            result.Add($"{root.val}");
+            return result;
+        }
+
+        string startPath = $"{root.val}";
+        if (root.left != null)
+            PathToLeaf(root.left, startPath, result);
+        if (root.right != null)
+            PathToLeaf(root.right, startPath, result);
+        return result;
+    }
+}
