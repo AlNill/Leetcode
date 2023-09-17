8b79396ea39deb848a236e835476b894758de5e6
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-17 10:56:06 -0700

    Good

---
+++ Easy/290.WordPattern/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace Easy._290.WordPattern;
+
+/*
+ * Beats 74.02% by speed and 34.31% by memory;
+ */
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        Dictionary<char, string> hashTable = new Dictionary<char, string>();
+        var words = s.Split(' ');
+        if (pattern.Length != words.Length)
+            return false;
+        for (int i = 0; i < pattern.Length; ++i)
+        {
+            if (!hashTable.ContainsKey(pattern[i]))
+            {
+                if (hashTable.ContainsValue(words[i]))
+                    return false;
+                hashTable.Add(pattern[i], words[i]);
+            }
+            else if (hashTable[pattern[i]] == words[i])
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+}
