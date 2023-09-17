a6e8e569fc90835eb49d30f7b7509d1e04f25574
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-17 11:22:22 -0700

    Very good

---
+++ Easy/268.MissingNumber/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace Easy._268.MissingNumber;
+
+/*
+ * Beats 94.22% by speed and 93.66% by memory
+ */
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int max = 0, current = 0;
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            current += nums[i];
+            max += i;
+        }
+        max += nums.Length;
+        return max - current;
+    }
+}
