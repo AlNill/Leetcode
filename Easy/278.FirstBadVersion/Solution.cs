c7a93b0872fb025749f7a721642ec1da5bcb1d5d
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-17 11:25:07 -0700

    Good by speed

---
+++ Easy/278.FirstBadVersion/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace Easy._278.FirstBadVersion;
+
+public class VersionControl
+{ }
+
+
+/*
+ * Beats 84.99% by speed and 37.30% by memory
+ */
+public class Solution : VersionControl
+{
+    /* The isBadVersion API is defined in the parent class VersionControl.
+          bool IsBadVersion(int version); */
+    public bool IsBadVersion(int n )
+    {
+        return false;
+    }
+
+    public int FirstBadVersion(int n)
+    {
+        int l = 1, r = n;
+        while (l <= r)
+        {
+            int m = l + ((r - l) / 2);
+            if (IsBadVersion(m - 1) && IsBadVersion(m))
+                r = m - 1;
+            else if (!IsBadVersion(m - 1) && !IsBadVersion(m))
+                l = m + 1;
+            else
+                return m;
+        }
+        return 0;
+    }
+}
