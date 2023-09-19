2b052b0855423d2e0ee8478bef1fb29f02891f7b
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-19 14:05:51 -0700

    Good

---
+++ Easy/349.IntersectionOfTwoArrays/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Easy._349.IntersectionOfTwoArrays;
+
+/*
+ * Beats 93.19% by speed and 84.33% by memory
+ */
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> result = new HashSet<int>();
+        for (int i = 0; i < nums1.Length; ++i)
+        {
+            if (nums2.Contains(nums1[i]))
+                result.Add(nums1[i]);
+        }
+        return result.ToArray();
+    }
+}
