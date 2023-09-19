1375c667f766d4a525495c1066f933c307ebaf94
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-19 14:07:54 -0700

    +-

---
+++ Easy/344.ReverseString/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Easy._344.ReverseString;
+
+/*
+ * Beats 66.76% by sped and 50.41% by memory
+ */
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int middle = s.Length / 2;
+        for (int i = 0; i < middle; ++i)
+        {
+            char tmp = s[s.Length - 1 - i];
+            s[s.Length - 1 - i] = s[i];
+            s[i] = tmp;
+        }
+    }
+}
