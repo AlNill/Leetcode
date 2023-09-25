43271e086d09da7af8a8582ab869c5204a038b5a
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-09-25 13:54:23 -0700

    Shorter solution

--- Easy/405.ConvertANumberToHexadecimal/Solution.cs
+++ Easy/405.ConvertANumberToHexadecimal/Solution.cs
@@ -8,41 +8,22 @@ namespace Easy._405.ConvertANumberToHexadecimal;
  */
 public class Solution
 {
-    private static Dictionary<uint, char> alphabet = new Dictionary<uint, char>()
-    {
-        {0, '0'},
-        {1, '1'},
-        {2, '2'},
-        {3, '3'},
-        {4, '4'},
-        {5, '5'},
-        {6, '6'},
-        {7, '7'},
-        {8, '8'},
-        {9, '9'},
-        {10, 'a'},
-        {11, 'b'},
-        {12, 'c'},
-        {13, 'd'},
-        {14, 'e'},
-        {15, 'f'}
-    };
+    private static string alphabet = "0123456789abcdef";
 
     public string ToHex(int num)
     {
         if (num == 0)
             return "0";
-
         uint tmp = (uint)num;
         StringBuilder sb = new StringBuilder();
         while (tmp > 15)
         {
             uint letterKey = tmp % 16;
-            sb.Insert(0, alphabet[letterKey]);
+            sb.Insert(0, alphabet[(int)letterKey]);
             tmp = tmp >> 4;
         }
         if (tmp != 0)
-            sb.Insert(0, alphabet[tmp]);
+            sb.Insert(0, alphabet[(int)tmp]);
         return sb.ToString();
     }
 }
