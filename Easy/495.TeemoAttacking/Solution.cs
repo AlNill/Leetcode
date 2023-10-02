f7009e52740b018bdd81107eed5e8f4bf5ba79bc
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-02 11:40:11 -0700

    Good

---
+++ Easy/495.TeemoAttacking/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace Easy._495.TeemoAttacking;
+
+/*
+ * Beats 88.44% by speed and 64.63% by memory
+ */
+public class Solution
+{
+    public int FindPoisonedDuration(int[] timeSeries, int duration)
+    {
+        int result = 0, prevPoisonAttack = timeSeries[0];
+        foreach (int second in timeSeries)
+        {
+            int time = second - prevPoisonAttack;
+            result += time > duration ? duration : time;
+            prevPoisonAttack = second;
+        }
+        result += duration;
+        return result;
+    }
+}
