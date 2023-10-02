bae46478869a4d0fb0f346d434fecdca6ff2bc23
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-02 11:26:26 -0700

    +-

---
+++ Easy/485.MaxConsecutiveOnes/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Easy._485.MaxConsecutiveOnes;
+
+/*
+ * Beats 70.41% by speed and 50.11% by memory
+ */
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int max = 0, current = 0;
+        foreach (int i in nums)
+        {
+            current = i == 0 ? 0 : ++current;
+            max = max > current ? max : current;
+        }
+        return max;
+    }
+}
