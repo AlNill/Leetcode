cb1f10f61391ae4d5a3cace8690242c290470da3
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-02 11:44:24 -0700

    +-

---
+++ Easy/492.ConstructTheRectangle/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace Easy._492.ConstructTheRectangle;
+
+/*
+ * Beats 78.89% by speed and 30% by memory
+ */
+public class Solution
+{
+    public int[] ConstructRectangle(int area)
+    {
+        int sqrt = (int)Math.Sqrt(area);
+        while (area % sqrt != 0)
+        {
+            --sqrt;
+        }
+        return new int[] { area / sqrt, sqrt };
+    }
+}
