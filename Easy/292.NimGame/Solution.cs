451545c4cbf73505c61324be32564e9564e31a80
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-17 10:07:00 -0700

    Good

---
+++ Easy/292.NimGame/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace Easy._292.NimGame;
+
+/*
+ * Beats 87.56% by speed and 67.28% by memory
+ */
+public class Solution
+{
+    public bool CanWinNim(int n)
+    {
+        return !(((n & 3) == 0));
+    }
+}
