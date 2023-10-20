9f6cf56133cd25949c5e852c5a9d6278c65b7045
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-20 10:53:18 -0700

    +-

---
+++ Easy/441.ArrangingCoins/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Easy._441.ArrangingCoins;
+
+/*
+ * Beats 93.81% by speed and 34.51% by memory
+ */
+public class Solution
+{
+    public int ArrangeCoins(int n)
+    {
+        long tmp = n;
+        return (int)((Math.Sqrt(8 * tmp + 1) - 1) / 2);
+    }
+}
