972db6ff33d76b680064e9b570e4cbeac521da9c
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-20 11:47:25 -0700

    Good by speed

---
+++ Easy/409.LongestPalindrome/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Easy._409.LongestPalindrome;
+
+/*
+ * Beats 92.95% by speed and 22.76% by memory
+ */
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        int result = 0;
+        HashSet<char> hashTable = new HashSet<char>();
+        for (int i = 0; i < s.Length; ++i)
+        {
+            if (hashTable.Contains(s[i]))
+            {
+                hashTable.Remove(s[i]);
+                result += 2;
+            }
+            else
+            {
+                hashTable.Add(s[i]);
+            }
+        }
+        return hashTable.Count == 0 ? result : result + 1;
+    }
+}
