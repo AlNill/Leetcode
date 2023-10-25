e348b4216155be4c44f745c79951191194010f3e
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-25 03:40:42 -0700

    Good

---
+++ Medium/24.SwapNodesInPairs/Solution.cs
@@ -0,0 +1,36 @@
+﻿using Medium.Common;
+
+namespace Medium._24.SwapNodesInPairs;
+
+/*
+ * Beats 89.87% by speed and 61.13% by memory
+ */
+public class Solution
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+
+        var start = head;
+        var next = head.next;
+        var newHead = next;
+
+        ListNode prevStart = null;
+        while (next != null)
+        {
+            start.next = next.next;
+            next.next = start;
+            if (prevStart != null)
+                prevStart.next = next;
+
+            if (next.next.next == null || next.next.next.next == null)
+                break;
+
+            prevStart = start;
+            next = next.next.next.next;
+            start = start.next;
+        }
+        return newHead;
+    }
+}
