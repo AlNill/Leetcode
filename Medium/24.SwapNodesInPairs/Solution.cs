8b43dddae873007b61bdfb15cf6647fd5a030c86
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-25 06:19:18 -0700

    add sorter solution but more slow

--- Medium/24.SwapNodesInPairs/Solution.cs
+++ Medium/24.SwapNodesInPairs/Solution.cs
@@ -2,11 +2,36 @@
 
 namespace Medium._24.SwapNodesInPairs;
 
-/*
- * Beats 89.87% by speed and 61.13% by memory
- */
+
 public class Solution
 {
+    /*
+     * Beats 6.73% by speed and 39.29% by memory
+     */
+    public ListNode ShorterSwapPairs(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+        var newHead = new ListNode();
+        var prev = newHead;
+        var current = head;
+
+        while(current != null && current.next != null)
+        {
+            prev.next = current.next;
+            current.next = current.next.next;
+            prev.next.next = current;
+
+            prev = current;
+            current = current.next;
+        }
+
+        return newHead.next;
+    }
+
+    /*
+    * Beats 89.87% by speed and 61.13% by memory
+    */
     public ListNode SwapPairs(ListNode head)
     {
         if (head == null || head.next == null)
