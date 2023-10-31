8dc0d6485643f11e9ac0b94d26c750e2a95342d9
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-10-31 13:15:05 -0700

    +-

---
+++ Easy/543.DiameterOfBinaryTree/Solution.cs
@@ -0,0 +1,26 @@
+﻿using Easy.Common;
+
+namespace Easy._543.DiameterOfBinaryTree;
+
+/*
+ * Beats 96.59% by speed and 48.99% by memory
+ */
+public class Solution
+{
+    private int dfs(TreeNode root, ref int d)
+    {
+        if (root == null)
+            return 0;
+        var left = dfs(root.left, ref d);
+        var right = dfs(root.right, ref d);
+        d = Math.Max(d, left + right);
+        return Math.Max(left, right) + 1;
+    }
+
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        int d = 0;
+        dfs(root, ref d);
+        return d;
+    }
+}
