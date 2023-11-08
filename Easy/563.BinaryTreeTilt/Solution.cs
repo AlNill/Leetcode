14f21c4a5e98c73e3d8fc4e025d2054491b6f23e
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-11-08 12:25:13 -0800

    Good

---
+++ Easy/563.BinaryTreeTilt/Solution.cs
@@ -0,0 +1,28 @@
+﻿using Easy.Common;
+
+namespace Easy._563.BinaryTreeTilt;
+
+/*
+ * Beats 81.25% by speed and 70.83% by memory
+ */
+public class Solution
+{
+    int max = 0;
+
+    public int FindTilt(TreeNode root)
+    {
+        CalcSum(root);
+        return max;
+    }
+
+    private int CalcSum(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        int leftSum = CalcSum(root.left);
+        int rightSum = CalcSum(root.right);
+        max += Math.Abs(rightSum - leftSum);
+        return leftSum + rightSum + root.val;
+    }
+}
