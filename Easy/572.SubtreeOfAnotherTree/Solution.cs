5e2b02e91037d20f2099f621a6b8e99c289bb97d
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-11-10 08:57:52 -0800

    +-

---
+++ Easy/572.SubtreeOfAnotherTree/Solution.cs
@@ -0,0 +1,28 @@
+﻿using Easy.Common;
+
+namespace Easy._572.SubtreeOfAnotherTree;
+
+/*
+ * Beats 81.28% by speed and 50.62% by memory
+ */
+public class Solution
+{
+    private bool Compare(TreeNode lRoot, TreeNode rRoot)
+    {
+        if (lRoot == null && rRoot == null)
+            return true;
+        if (lRoot == null || rRoot == null || lRoot.val != rRoot.val)
+            return false;
+
+        return Compare(lRoot.left, rRoot.left) && Compare(lRoot.right, rRoot.right);
+    }
+
+    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    {
+        if (root == null)
+            return false;
+        if (Compare(root, subRoot))
+            return true;
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+}
