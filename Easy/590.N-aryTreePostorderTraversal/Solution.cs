b714c55629520f41b1c56a4b734e117c79ebaeef
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-11-11 01:12:20 -0800

    BEETS 100% BY SPEED

---
+++ Easy/590.N-aryTreePostorderTraversal/Solution.cs
@@ -0,0 +1,29 @@
+﻿using Easy.Common;
+
+namespace Easy._590.N_aryTreePostorderTraversal;
+
+/*
+ * Beats 100% by speed and 21.49% by memory
+ */
+public class Solution
+{
+    private IList<int> _result = new List<int>();
+
+    public IList<int> Postorder(Node root)
+    {
+        FillPostorder(root);
+        return _result;
+    }
+
+    private void FillPostorder(Node root)
+    {
+        if (root == null)
+            return;
+
+        foreach (var child in root.children)
+        {
+            FillPostorder(child);
+        }
+        _result.Add(root.val);
+    }
+}
