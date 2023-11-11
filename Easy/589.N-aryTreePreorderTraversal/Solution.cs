fe20205438da1d6ae44498af855516f7ed2911d9
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-11-11 01:09:49 -0800

    +-

---
+++ Easy/589.N-aryTreePreorderTraversal/Solution.cs
@@ -0,0 +1,27 @@
+﻿using Easy.Common;
+
+namespace Easy._589.N_aryTreePreorderTraversal;
+
+/*
+ * Beats 74.51% by speed and 58.17% by memory
+ */
+public class Solution
+{
+    private IList<int> _result = new List<int>();
+    public IList<int> Preorder(Node root)
+    {
+        FillPreorder(root);
+        return _result;
+    }
+
+    private void FillPreorder(Node root)
+    {
+        if (root == null)
+            return;
+        _result.Add(root.val);
+        foreach (var child in root.children)
+        {
+            FillPreorder(child);
+        }
+    }
+}
