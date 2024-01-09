d60f0a5d3c4ff259bc662333a7efd48e41f16391
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-01-09 09:31:11 -0800

    Good. BEATS 100% by speed.

---
+++ Easy/693.BinaryNumberWithAlternatingBits/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Easy._693.BinaryNumberWithAlternatingBits;
+
+/*
+ * Beats 100% by speed and 60% by memory
+ */
+public class Solution
+{
+    public bool HasAlternatingBits(int n)
+    {
+        n = n ^ (n >> 1);
+        return (n & n + 1) == 0;
+    }
+}
