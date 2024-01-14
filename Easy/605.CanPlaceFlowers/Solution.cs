fa35b06f3e0422ebaf556da748280bc452a6c06e
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-01-14 13:36:40 -0800

    Good by speed.

---
+++ Easy/605.CanPlaceFlowers/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Easy._605.CanPlaceFlowers;
+
+/*
+ * Beats 97.16% by spped and 33.10% by memory
+ */
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        if (n == 0)
+            return true;
+
+        for (int i = 0; i < flowerbed.Length; ++i)
+        {
+            if ((i == 0 || flowerbed[i - 1] == 0) &&
+               (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0) &&
+                flowerbed[i] == 0)
+            {
+                --n;
+                flowerbed[i] = 1;
+                if (n == 0)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
