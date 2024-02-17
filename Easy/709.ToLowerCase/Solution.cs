4a73308eed558fe756b03cb009e2ab8f11bb42fb
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-02-17 06:51:38 -0800

    Good

---
+++ Easy/709.ToLowerCase/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Easy._709.ToLowerCase;
+
+/*
+ * Beats 90.91% by speed and 52.96% by memory
+ */
+public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.Length; ++i)
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                result.Append((char)(s[i] + 32));
+            else
+                result.Append(s[i]);
+        }
+        return result.ToString();
+    }
+}
