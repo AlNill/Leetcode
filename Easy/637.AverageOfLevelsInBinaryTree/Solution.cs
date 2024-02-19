baefe8ca2e5f38e7b5b633b8332bf245751fcfed
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-02-19 10:43:02 -0800

    Very good

---
+++ Easy/637.AverageOfLevelsInBinaryTree/Solution.cs
@@ -0,0 +1,31 @@
+﻿using Easy.Common;
+
+namespace Easy._637.AverageOfLevelsInBinaryTree;
+
+/*
+ * Beats 91.28% by speed and 98.55% by memory
+ */
+public class Solution
+{
+    public IList<double> AverageOfLevels(TreeNode root)
+    {
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        List<double> result = new List<double>();
+        queue.Enqueue(root);
+        while (queue.Count != 0)
+        {
+            long tmp = 0, n = queue.Count;
+            for (int i = 0; i < n; ++i)
+            {
+                TreeNode t = queue.Dequeue();
+                if (t.left != null)
+                    queue.Enqueue(t.left);
+                if (t.right != null)
+                    queue.Enqueue(t.right);
+                tmp += t.val;
+            }
+            result.Add((double)tmp / n);
+        }
+        return result;
+    }
+}
