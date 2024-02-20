0008b5b5b3e3074b2008f1d5c83d14a51fff433b
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-02-20 11:37:54 -0800

    +-

---
+++ Easy/680.ValidPalindromeII/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace Easy._680.ValidPalindromeII;
+
+/*
+ * Beats 73.19% by speed and 47.94% by memory
+ */
+public class Solution
+{
+    public bool ValidPalindrome(string s)
+    {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return IsValidPalindrome(s, left, right - 1) || IsValidPalindrome(s, left + 1, right);
+            ++left;
+            --right;
+        }
+        return true;
+    }
+
+    private bool IsValidPalindrome(string s, int left, int right)
+    {
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+            ++left;
+            --right;
+        }
+        return true;
+    }
+}
