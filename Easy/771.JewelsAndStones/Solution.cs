8d050b21a38cc31fc583a36b8d88c51053d84b8a
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-04-23 20:46:50 -0700

    Good by speed

---
+++ Easy/771.JewelsAndStones/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Easy._771.JewelsAndStones;
+
+/*
+ * Beats 96.22% by speed and 33.89% by memory
+ */
+public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        int totalCount = 0;
+        HashSet<char> jewelsSet = new HashSet<char>();
+        for (int i = 0; i < jewels.Length; ++i)
+        {
+            jewelsSet.Add(jewels[i]);
+        }
+
+        for (int i = 0; i < stones.Length; ++i)
+        {
+            if (jewelsSet.Contains(stones[i]))
+                ++totalCount;
+        }
+        return totalCount;
+    }
+}
