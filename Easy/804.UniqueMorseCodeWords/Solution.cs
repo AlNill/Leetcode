71738096acb1fc87142792e0beef2ededf58091f
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-04-25 12:18:07 -0700

    Good

---
+++ Easy/804.UniqueMorseCodeWords/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace Easy._804.UniqueMorseCodeWords;
+
+/*
+ * Beats 99.40% by speed and 91.07% by memory
+ */
+public class Solution
+{
+    private HashSet<string> _hashTable;
+    private Dictionary<char, string> _alphabet = new Dictionary<char, string>()
+    {
+        { 'a', ".-"},
+        { 'b', "-..."},
+        { 'c', "-.-."},
+        { 'd', "-.."},
+        { 'e', "."},
+        { 'f', "..-."},
+        { 'g', "--."},
+        { 'h', "...."},
+        { 'i', ".."},
+        { 'j', ".---"},
+        { 'k', "-.-"},
+        { 'l', ".-.."},
+        { 'm', "--"},
+        { 'n', "-."},
+        { 'o', "---"},
+        { 'p', ".--."},
+        { 'q', "--.-"},
+        { 'r', ".-."},
+        { 's', "..."},
+        { 't', "-"},
+        { 'u', "..-"},
+        { 'v', "...-"},
+        { 'w', ".--"},
+        { 'x', "-..-"},
+        { 'y', "-.--"},
+        { 'z', "--.."}
+    };
+
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        _hashTable = new HashSet<string>();
+
+        for (int i = 0; i < words.Length; ++i)
+        {
+            string word = words[i];
+            string code = "";
+            for (int j = 0; j < word.Length; ++j)
+            {
+                code += _alphabet[word[j]];
+            }
+            _hashTable.Add(code);
+        }
+
+        return _hashTable.Count;
+    }
+}
