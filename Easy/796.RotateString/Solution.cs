7eda446986f1a09c4af82caa0211ffeef4a7318b
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-04-24 20:53:09 -0700

    +-

---
+++ Easy/796.RotateString/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Easy._796.RotateString;
+
+/*
+ * Beats 89.88% by speed and 36.31% by memory
+ */
+public class Solution
+{
+    public bool RotateString(string s, string goal)
+    {
+        if (goal == s)
+            return true;
+        var tmp = s.Substring(1) + s[0];
+        while (tmp != s)
+        {
+            if (tmp == goal)
+                return true;
+            tmp = tmp.Substring(1) + tmp[0];
+        }
+        return false;
+    }
+}
